6130577ac12b49b4a16ffa5f5e48ff39dcc6f36b
Advaith <advaith.narayanan20@gmail.com>
2024-06-12 08:42:10 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# AL Updation
+A package for generating parametric design data optimally using Batch Active Learning.
+
+## Quick Start
+Coming soon
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='al_for_parametric_design_datasets',
+    version='0.0.1',
+    author='Advaith Narayanan',
+    author_email='advaith.narayanan20@gmail.com',
+    description='A library for optimally generating data with batch active learning in design contexts',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=['ALUpdation'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'tf',
+        'scikit-learn',
+        'matplotlib',
+        'sobol_seq'
+    ]
+) 
